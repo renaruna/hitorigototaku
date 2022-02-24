0b41c87dc3d5e7d91eaa58e1a1710842a8c50efe
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-25 01:15:26 +0900

    Update character.js

--- character.js
+++ character.js
@@ -1,32 +1,28 @@
 $(function() {
+    //menu右から表示
     var $home = $('.home-index');
     var h = $(window).height();
 
-    $('.menu').hover(
-        function(){
-            $home.animate({'marginRight':'380px'},500);
-            $home.css("height",h);
-        },
-        function () {
-          $home.animate({'marginRight':'0'},500);
-        }
-    );
+    $('.menu').click(function() {
+        $home.animate({'marginRight':'380px'},500);
+        $home.css("height",h);
+    });
     
     //スライダー
     function toggleChangeBtn() {
-    var slideIndex = $('.slide').index($('.active'));
-    //前へ次へボタンは表示しとく
-    $('.change-btn').show();
-    
-    if (slideIndex == 0) {
-      //activeになってるスライドが1番最初（0）のとき、前へボタンを隠す
-      $('.prev-btn').hide();
-    
-    } else if (slideIndex == $('.slide').length - 1) {
-      //activeになってるスライドが1番最後（slide.length-1）のとき、次へボタンを隠す
-      $('.next-btn').hide();
+        var slideIndex = $('.slide').index($('.active'));
+        //前へ次へボタンは表示しとく
+        $('.change-btn').show();
+
+        if (slideIndex == 0) {
+          //activeになってるスライドが1番最初（0）のとき、前へボタンを隠す
+          $('.prev-btn').hide();
+
+        } else if (slideIndex == $('.slide').length - 1) {
+          //activeになってるスライドが1番最後（slide.length-1）のとき、次へボタンを隠す
+          $('.next-btn').hide();
+        }
     }
-  }
   
   //下の番号を押して写真変える
   $('.index-btn').click(function() {
