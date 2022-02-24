5c0f1df7fb0a2e02473d37efd982c296b7864a2d
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-24 22:42:33 +0900

    Update character.js

--- character.js
+++ character.js
@@ -11,4 +11,40 @@ $(function() {
           $home.animate({'marginRight':'0'},500);
         }
     );
+    
+    //スライダー
+    function toggleChangeBtn() {
+    var slideIndex = $('.slide').index($('.active'));
+    //前へ次へボタンは表示しとく
+    $('.change-btn').show();
+    
+    if (slideIndex == 0) {
+      //activeになってるスライドが1番最初（0）のとき、前へボタンを隠す
+      $('.prev-btn').hide();
+    
+    } else if (slideIndex == $('.slide').length - 1) {
+      //activeになってるスライドが1番最後（slide.length-1）のとき、次へボタンを隠す
+      $('.next-btn').hide();
+    }
+  }
+  
+  //下の番号を押して写真変える
+  $('.index-btn').click(function() {
+    $('.active').removeClass('active');
+    var clickedIndex = $('.index-btn').index($(this));
+    $('.slide').eq(clickedIndex).addClass('active');
+    toggleChangeBtn();
+  });
+  
+  //前へ次へボタン押して写真変える
+  $('.change-btn').click(function() {
+    var $displaySlide = $('.active');
+    $displaySlide.removeClass('active');
+    if ($(this).hasClass('next-btn')) {
+      $displaySlide.next().addClass('active');
+    } else {
+      $displaySlide.prev().addClass('active');
+    }
+    toggleChangeBtn();
+  });
 });
