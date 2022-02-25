6d7ac62556ed96f188108baccf8d2f7077304321
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-26 01:11:30 +0900

    Update manager.js

--- manager.js
+++ manager.js
@@ -47,21 +47,21 @@ function convertArray(dataC, dataS) {
 
 function display() {
     let sKP = "";
-    let sPL = "";
+    let sPL = "<table>";
 
-    for (let i = 0; i < sArray.length; i++) {
+    for (let i = 1; i <= sArray.length; i++) {
         if (sArray[i][5].indexOf("管理人") !== -1) {
             if (sArray[i][0]) {
                 sKP += '<a href="'+sArray[i][12]+'">'+change(sArray[i][1], "#", ",")+'</a><br>';
             }
         }
         if (sArray[i][7]) {
-            sPL += '<td><a href="'+sArray[i][12]+'">'+sArray[i][1]+'</a></td><td><a href="'+search(sArray[i][7])+'">'+sArray[i][7]+'</a></td></tr><tr>';
+            sPL += '<tr><td><a href="'+sArray[i][12]+'">'+change(sArray[i][1], "#", ",")+'</a></td><td><a href="'+search(sArray[i][7])+'">'+sArray[i][7]+'</a></td></tr>';
         }
     }
 
     kpElement.innerHTML = sKP;
-    plElement.innerHTML = sPL.slice(0, -9);
+    plElement.innerHTML = sPL+'</table>';
 }
 
 function search(chara) {
