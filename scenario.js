5b71c51f7b34193727db2381f3a9c74bea291793
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-25 20:57:14 +0900

    playtime 複数陣に対応

--- scenario.js
+++ scenario.js
@@ -221,6 +221,7 @@ function display() {
             let dataObject = {};
             dataObject.id = sArray[scenarioID + j][0];
             dataObject.date = sArray[scenarioID + j][4];
+            dataObject.time = sArray[scenarioID + j][14];
             dataObject.KP = sArray[scenarioID + j][5];
             dataObject.driver = sArray[scenarioID + j][6];
             dataObject.manager = sArray[scenarioID + j][7];
@@ -284,13 +285,14 @@ function display() {
                 creator = '<p><a href="'+search(crArray, dataObject.creator)+'">'+dataObject.creator+'</a>：<a href="creator.html">創造主</a></p>';
             }
             //HTML
-            text += '<h2>第'+(j+1)+'陣</h2></div><div class="item"><table><tr><th>プレイした日付</th><td><p>'+dataObject.date+'</p></td></tr><tr><th>KP</th><td><p>'+kpHtml+'</p></td></tr><tr><th>CAST</th><td><div><p>PC</p>'+driver+manager+boss+spiritual+warcrim+creator+'<p>'+dataObject.elsepc+'</p></div></td></tr></table></div><div>';
+            text += '<h2>第'+(j+1)+'陣</h2></div><div class="item"><table><tr><th>プレイした日付</th><td><p>'+dataObject.date+'</p></td></tr><tr><th>プレイ時間</th><td><p>'+dataObject.time+'</p></td></tr><tr><th>KP</th><td><p>'+kpHtml+'</p></td></tr><tr><th>CAST</th><td><div><p>PC</p>'+driver+manager+boss+spiritual+warcrim+creator+'<p>'+dataObject.elsepc+'</p></div></td></tr></table></div><div>';
         }
         text += '<h2>第'+jin+'陣</h2>';
         jinElement.innerHTML = text;
         let dataObject = {};
         dataObject.id = sArray[scenarioID + jin - 1][0];
         dataObject.date = sArray[scenarioID + jin - 1][4];
+        dataObject.time = sArray[scenarioID + jin - 1][14];
         dataObject.KP = sArray[scenarioID + jin - 1][5];
         dataObject.driver = sArray[scenarioID + jin - 1][6];
         dataObject.manager = sArray[scenarioID + jin - 1][7];
@@ -303,6 +305,7 @@ function display() {
         
         //date
         dateElement.textContent = dataObject.date;
+        playTimeElement.textContent = dataObject.time;
         //KP
         let memberKP;
         let kpArray = [];
