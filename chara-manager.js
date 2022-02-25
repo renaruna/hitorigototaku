fdf96ddb1153e87fd174a838a42b32e05c966ca0
renaruna <79509944+renaruna@users.noreply.github.com>
2022-02-25 19:43:52 +0900

    Update chara-manager.js

--- chara-manager.js
+++ chara-manager.js
@@ -119,7 +119,19 @@ function display() {
     sexElement.textContent = dataObject.sex;
     jobElement.textContent = dataObject.job;
     memberElement.textContent = dataObject.member;
+    
+    let colorBack;//背景色にキャラの色
+    let colorMoji;//背景色にあった文字の色（#888か白）
+    let colorNum = dataObject.color.indexOf("#");
+    if (colorNum !== -1) {
+      colorBack = dataObject.color.substr(colorNum,7);
+    } else {
+      colorBack = "#fffff0";
+    }
+    colorMoji = blackOrWhite(colorBack);
     colorElement.textContent = dataObject.color;
+    colorElement.style.backgroundColor = colorBack;
+    colorElement.style.color = colorMoji;
     makerElement.textContent = dataObject.maker;
     
     hpElement.textContent = dataObject.hp;
@@ -175,6 +187,14 @@ function change(text, a, b) {
     text = text.replace(a,b); 
   }
   return text;
+}
+
+function blackOrWhite(hexcolor) {
+	var r = parseInt(hexcolor.substr(1, 2), 16) ;
+	var g = parseInt(hexcolor.substr(3, 2), 16) ;
+	var b = parseInt(hexcolor.substr(5, 2), 16) ;
+
+	return ( ( ( (r * 299) + (g * 587) + (b * 114) ) / 1000 ) < 128 ) ? "white" : "#888" ;
 }
    
 getCsvData('character-index - manager.csv', 'website - scenario.csv');
